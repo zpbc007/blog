a070b369d3ee1cd720f924fa16088f663832a58f
zhaopeng <719197352@qq.com>
2018-03-22 16:50:01 +0800

    koa 测试成功

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ require('module-alias/register')
 const path = require('path')
 const showdown = require('showdown')
 const readFile = require('@util/file.js').readFile
+const Koa = require('koa')
+const app = new Koa()
 
 const converter = new showdown.Converter()
 
 async function getHtml () {
     const file = await readFile(path.join(__dirname, './docs/1.log.md'))
-    console.log(converter.makeHtml(file))
+    return converter.makeHtml(file)
 }
 
-getHtml()
 
+
+app.use(async ctx => {
+    ctx.body = await getHtml()
+})
+
+app.listen('3000')
 
