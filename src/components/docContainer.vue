8ed50b4aa75f87e57212fcabb3cc1871d5545774
zhaopeng <719197352@qq.com>
2018-03-31 13:07:33 +0800

    markdown支持流程图

--- src/components/docContainer.vue
+++ src/components/docContainer.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="markdown-style" v-append="innerHtml">
+    <div>
+        <div class="markdown-style doc-container" v-html="innerHtml">
+    </div>
     </div>
 </template>
 <script>
+import flowchart from 'flowchart.js'
 import axios from 'axios'
 import '../../static/css/code.css'
 import '../../static/css/markdown.css'
@@ -19,6 +22,20 @@ export default {
              innerHtml: '暂无数据'
         }
     },
+    watch: {
+        // 如果有流程图 利用flowchart进行转换
+        innerHtml () {
+            this.$nextTick(() => {
+                let flowCode = document.querySelector('.language-flow')
+                if (flowCode) {
+                    flowCode.setAttribute('id', 'flow-chart')
+                    let flowChart = flowchart.parse(flowCode.innerText)
+                    flowCode.innerText = ''
+                    flowChart.drawSVG('flow-chart')
+                }
+            })
+        }
+    },
     methods: {
         getDoc () {
             axios.get(`/api/view/${this.id}`)
@@ -32,4 +49,8 @@ export default {
     }
 }
 </script>
-
+<style>
+.doc-container {
+    overflow-x: auto;
+}
+</style>
