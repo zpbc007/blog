8cbcb90b0478be47e43dd5cd854ce0ca06ac899e
zhaopeng <719197352@qq.com>
2018-04-02 11:37:35 +0800

    更改端口

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const Koa = require('koa')
 const staticServe = require('koa-static');
 const app = new Koa()
 const route = require('./middleware/router').router
+const port = '80'
 
 // 控制台
 const spinner = ora('system is starting now')
@@ -19,6 +20,6 @@ app.use(staticServe(path.resolve(__dirname, './dist')))
 // 路由
 app.use(route)
 
-app.listen('8088')
+app.listen(port)
 spinner.stop()
-console.log(chalk.cyan('系统启动成功, 监听端口为8088\n'))
+console.log(chalk.cyan(`系统启动成功, 监听端口为${port}\n`))
