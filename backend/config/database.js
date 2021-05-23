22371d17a0c8bdc6441dc825036337ba6a0499c6
ubuntu <ubuntu@localhost.localdomain>
2021-05-23 14:34:25 +0800

    config

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+        client: 'postgres',
+        host: env('db_host', 'localhost'),
+        port: env.int('db_port', 5432),
+        database: env('db_name', 'strapi'),
+        username: env('db_user', 'strapi'),
+        password: env('db_password', 'strapi'),
       },
     },
   },
