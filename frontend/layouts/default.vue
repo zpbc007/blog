7db0db77ec21f379d5e39e3351c867e5c8e3c5ff
zhaopeng06 <zhaopeng06@kuaishou.com>
2021-07-25 16:48:07 +0800

    fix: cover

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -37,13 +37,7 @@
           class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle"
           uk-grid
         >
-          <div
-            class="uk-background-cover"
-            style="
-              background-image: url('https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3308&q=80 3308w');
-            "
-            uk-height-viewport
-          />
+          <div class="uk-background-cover cover" uk-height-viewport />
           <div class="uk-padding-large">
             <h1 style="font-family: Staatliches">Strapi blog</h1>
             <div class="uk-width-1-2@s">
@@ -74,12 +68,17 @@
 <script>
 export default {
   async fetch() {
-    this.categories = await this.$strapi.find("categories");
+    this.categories = await this.$strapi.find('categories')
   },
   data: function () {
     return {
       categories: [],
-    };
+    }
   },
-};
+}
 </script>
+<style lang="css" scoped>
+.cover {
+  background-image: url('https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3308&q=80');
+}
+</style>
